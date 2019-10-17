b65edcdc7557df556931eb40916a086ac3562de7
Daniel Herrero <danielherrerohernando@gmail.com>
2019-10-17 16:30:59 +0200

    fixing css template

--- generators/app/templates/react-app/src/App.js
+++ generators/app/templates/react-app/src/App.js
@@ -1,23 +1,23 @@
-import React from "react";
+import React from 'react'
 
-import "./App.css";
+import './App.css'
 
 const App = () => {
-  return (
-    <div id="<%= name %>">
-      <div className="<%= name %>-app">
-        <header className="<%= name %>-app-header">
-          <h1>Micro-frontend template</h1>
-        </header>
-        <div className="<%= name %>-app-body">
-          <p>Testing this microfrontend stuff</p>
-          <button type="button" onClick={() => {}}>
-            Do sth
-          </button>
+    return (
+        <div className="<%= name %>">
+            <div className="<%= name %>-app">
+                <header className="<%= name %>-app-header">
+                    <h1>Micro-frontend template</h1>
+                </header>
+                <div className="<%= name %>-app-body">
+                    <p>Testing this microfrontend stuff</p>
+                    <button type="button" onClick={() => {}}>
+                        Do sth
+                    </button>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
-};
+    )
+}
 
-export default App;
+export default App
